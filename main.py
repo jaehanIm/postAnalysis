12173f74a02f1a30948a255cec0dd4e9da6ae39c
jaehan <jaehan.im@nearthlab.com>
2022-09-14 14:43:03 +0900

    update 0914

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ import matplotlib.pyplot as plt
 # gdLogAddr = sys.argv[1]
 
 gdLogAddr = "gdLog_220825_113829.csv";
-data = pd.read_csv(gdLogAddr)
 
-print(data.shape)
-print(data.gimbalRpy_deg_0)
+rootAddr = "root_220825_113829.txt"
+
+data = pd.read_csv(gdLogAddr)
+rootLog = open(rootAddr,"r")
+#test = rootLog.readline(2)
+
+for i in range(5):
+    test = rootLog.readline(i)
+    print(test)
+
+#print(data.shape)
+#print(data.gimbalRpy_deg_0)
 
 time = data.rosTime - data.rosTime[0]
 
@@ -28,16 +37,15 @@ thresholdList = [1,2,1]
 
 recordList = np.zeros([len(inspectThis),len(data)])
 
-
 # Check abnormality
 for i in range(len(data)):
     for dat in range(len(inspectThis)):
         if inspectThis[dat][i] >= thresholdList[dat]:
             recordList[dat][i] = 1
-            
+
 plt.figure(2)
 plt.plot(time,recordList[0])
 plt.plot(time,recordList[1])
 plt.plot(time,recordList[2])
-     
+
 # %%
